97b9bd265e9e355ec81ec5b6f21a7b888e5c9b3b
RyanNFreeman <ryan.norman.freeman@gmail.com>
2019-03-26 14:16:36 -0600

    got me an empty array

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const knexConfig = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  connection: {
+    filename: './data/lambda.sqlite3',
+  },
+};
+
+const db = knex(knexConfig);
 
 const server = express();
 
@@ -7,6 +18,16 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+server.get('/', (req, res) => {
+  db('zoos')
+  .then(roles => {
+    res.status(200).json(roles)
+  })
+  .catch(err => {
+    res.status(500).json(err)
+  })
+})
 
 const port = 3300;
 server.listen(port, function() {
