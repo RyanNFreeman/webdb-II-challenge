b0f1154a81cf9a47c35dfbb7a60458e16b69a9f9
RyanNFreeman <ryan.norman.freeman@gmail.com>
2019-03-26 15:26:41 -0600

    completed stretch. added bears router and posted/updated/deleted bears.

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const helmet = require('helmet');
 const zoosRouter = require('./router/zoos-router')
-
+const bearsRouter = require('./router/bears-router')
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
 server.use('/api/zoos', zoosRouter);
+server.use('/api/bears', bearsRouter);
 
 const port = 3300;
 server.listen(port, function() {
